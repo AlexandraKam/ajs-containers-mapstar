efb85f3d85d107c5ffe537c08e1bd033853ab302
Aleksandra Kameneva <kameneva_fkp@mail.ru>
2022-08-19 15:01:55 +0500

    ДЗ 8

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,5 +2,6 @@ import Settings from '../app';
 
 test('Изменение настроек по умолчанию', () => {
   const newSettings = new Settings();
-  expect(Array.from(newSettings.settings)).toEqual([['theme', 'dark'], ['music', 'trance'], ['difficulty', 'easy']]);
+  newSettings.userSettings = [['theme', 'light'], ['music', 'trance'], ['difficulty', 'nightmare']];
+  expect(Array.from(newSettings.settings)).toEqual([['theme', 'light'], ['music', 'trance'], ['difficulty', 'nightmare']]);
 });
